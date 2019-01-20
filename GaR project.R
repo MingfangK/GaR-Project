46d789606fe0728ade32f3e5a4697343fc663735
Miguel Arquez <miguel.arquez12@gmail.com>
2019-01-20 08:50:51 -0500

    GFSR data

--- GaR project.R
+++ GaR project.R
@@ -4,10 +4,24 @@
 library("dplyr")
 library("ggplot2")
 library("tsa")
+library("readxl")
+
+
+setwd("C:/Users/Miguel Arquez Abdala/Documents/GitHub/GaR-Project")
+
+FCI_index <- read_excel("gfsr-financial-conditions-indices.xlsx", sheet = 2,
+                            col_names = TRUE)
+
+FCI_col <- FCI_index %>%
+              select(date, COL)
+
+ggplot(data = FCI_col, aes(x = date, y = COL)) + 
+        geom_line() + ggtitle("Indice de condiciones financieras Colombia 1991-2016")
 
 
 
-# Change in the script
+
+
 
 
 
